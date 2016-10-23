ddf8f849527dc5d194a67127a1ca32bfbb9f695d
Administrator <Administrator@MS-20160614BMIE>
2016-10-23 11:39:33 +0800

    增加flip.js,用以添加课程介绍框动画

---
+++ ContosoUniversity/Scripts/flip.js
@@ -0,0 +1,20 @@
+﻿
+$(function () {
+    var $obj1 = $(".clour1");   
+    var $obj2 = $(".clour2");    
+    var $obj3 = $(".clour3");
+    addflip($obj1);
+    addflip($obj2);
+    addflip($obj3);
+});
+function addflip(obj) {
+    obj.hover(function () {
+        flip(obj);
+    });
+};
+function flip(obj) {
+    obj.addClass('flipInY');
+    obj.addClass('animated');
+    //function为闭包，用于将$(".clour")传进程序延迟执行
+    setTimeout(function () { obj.removeClass('flipInY'); }, 1000);
+};
